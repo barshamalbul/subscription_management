af9df4f8c7aaf3ada0c8d40c2457a60fe43622a8
barsha malbul <barshamalbul@gmail.com>
2019-11-11 10:46:15 +0545

    subscription plans cancelled correction

--- app/Http/Controllers/plansController.php
+++ app/Http/Controllers/plansController.php
@@ -15,53 +15,48 @@ class plansController extends Controller
         $plans = plans::all();
         $user_id=Auth::user()->id;
         $subscription = DB::table('subscriptions')->where([['user_id',$user_id],['stripe_status','active']])->get();
-
         return view('plans.index', compact('plans','subscription'));
     }
     
-
     public function store($id)
     {
         $user_id=Auth::user()->id;
         $user_name=Auth::user()->name;
         $plans=DB::table('plans')->where('id',$id)->first();
-       
-        $data=array(
-            $stripe_name=$plans->name,
-            $stripe_plan =$plans->stripe_plan,
-        );
-       
         $user = User::find(Auth::user()->id);
         $user->createAsStripeCustomer();
-        
-        //or use session to store name and plan
          return view('update-payment-method', [
             'intent' => $user->createSetupIntent()
         ])->with(compact('plans'));
-        //return redirect(route('home'));
     }
+
     public function subscribe(Request $request)
     {
+       
         $stripeToken=$request->stripeToken;
         $stripe_name=$request->stripe_name;
         $stripe_plan = $request->stripe_plan;
         $user = User::find(Auth::user()->id);
         $id=Auth::user()->id;
-        $subscription=DB::table('subscriptions')->where([['user_id',$id],['stripe_status','active']])->get();
-                    if ($user->subscribed($stripe_name)) 
-                    {       
-                        foreach($subscription as $sub) 
-                        {      
-                            if(($sub->stripe_status) == 'active')
+        $subscription=DB::table('subscriptions')->where([['user_id',$id],['name',$stripe_name],['stripe_status','active']])->first();
+        //dd( $subscription);           
+                    if ($user->subscribed($stripe_name) ) 
+                    {
+                        if(!$subscription == null) 
+                        {
+                            if(($subscription->stripe_status) == 'active')
                             {
-                                return redirect(route('home'))->with('success','you are already suscribed to '.$stripe_name." ".'plans'); 
+                                return redirect(route('home'))->with('success','you are already suscribed to '.$stripe_name." ".'plan'); 
                             }
-                        }
+                        }     
+
                     }
                     else
                     {
+                        echo($subscription);
                         $subscription=$user->newSubscription( $stripe_name,$stripe_plan )->create($stripeToken);
                         return redirect(route('home'))->with('success','sucessfully suscribed to '.$stripe_name." ".'plans');
+                    // return "there";
                     }
     }
 }
